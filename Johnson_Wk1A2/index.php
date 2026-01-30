55e043d879c2ba11c6bcda8e75a0fddd1bff6370
hosjoh <hosjoh4457@students.ecpi.edu>
2026-01-30 01:04:57 -0500

    Updated index.php

---
+++ Johnson_Wk1A2/index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Welcome to PHP!</title>
+</head>
+
+<body>
+    <?php echo '<h1 style="text-align: center;">Welcome to PHP!</h1>'?>
+    <?php echo '<h2 style="text-align: center;">My Name is Hosea Johnson</h2>' ?>
+    <?php echo '<p style="text-align: center;">The Date is 1/29/26</p>' ?>
+    <?php echo '<p style="text-align: center;">The Time 10:58PM</p>' ?>
+    <?php echo '<p style="text-align: center;">Created by Hosea Johnson</p>' ?>
+</body>
+
+</html>
